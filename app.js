649dc4cb0d6e9f895cf68bd80073909580989e27
Alistair Blake <consultations@me.com>
2018-11-14 19:22:12 -0800

    nothing new

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ var sugarCookieRecipe = {
         'Bake 7 to 12 minutes. Cool on wire rack.'],
 
 }
+
 var oatmealCookieRecipe = {
     id: 'oatmeal_cookie',
     name: 'Oatmeal Cookie',
@@ -67,63 +68,71 @@ var oatmealCookieRecipe = {
         'Drop cookies in 1 inch balls onto a greased cookie sheet',
         'Bake 10 to 12 minutes. Cool on wire wrack'
     ],
+
 }
 //this is the array that will collect all of the add-ons made by the construtor function
 var addOn = [];
+
 //constructor for creating add ons. this constructor will be pushed into add on array. It includes a name, and id, the instructions(addDIrections) and the amount
-function AddOn(name, id, addDirections, amount, number) {
+function AddOn(name, id, addDirections, amount) {
     this.name = name;
     this.id = id;
     this.addDirections = addDirections;
     this.amount = amount;
-    this.number = number;
     //push objects into array
     addOn.push(this);
 }
 //this functions holds all the new add ons that go into the array
 function makeAddOns() {
-    var chocolate_chip = new AddOn('chocolate_chip', 'chocolate_chip', 'Mix in Chocolate Chips', '1 cup', 0);
-    var coconut = new AddOn('coconut', 'coconut', 'Mix in Coconut', '1 cup', 1);
-    var peanut_butter = new AddOn('peanut_butter', 'peanut_butter', 'Mix in Peanut Butter until smooth', '1 cup', 2);
-    var cinnamon = new AddOn('cinnamon', 'cinnamon', 'Mix in Cinnamon', '1 tsp', 3);
-    var sprinkles = new AddOn('sprinkles', 'sprinkles', 'Sprinkle the sprinkles over the uncooked cookies', '1/2 cup', 4);
-    var frosting = new AddOn('frosting', 'frosting', 'Once cool, add frosting', '1 can', 5);
-    var nuts = new AddOn('nuts', 'nuts', 'Mix in Nuts', '1 cup', 6);
-    //TODO: add more options
+    var chocolate_chip = new AddOn('Chocolate Chip', 'chocolate_chip', 'Mix in Chocolate Chips', '1 cup');
+    var coconut = new AddOn('Coconut', 'Coconut', 'Mix in Coconut', '1 cup');
+    var peanut_butter = new AddOn('Peanut Butter', 'peanut_butter', 'Mix in Peanut Butter until smooth', '1 cup');
+    var cinnamon = new AddOn('Cinnamon', 'cinnamon', 'Mix in Cinnamon', '1 tsp');
+    var sprinkles = new AddOn('Sprinkles', 'sprinkles', 'Sprinkle the sprinkles over the uncooked cookies', '1/2 cup');
+    var frosting = new AddOn('Frosting', 'frosting', 'Once cool, add frosting', '1 can');
+    var nuts = new AddOn('Nuts', 'nuts', 'Mix in Nuts', '1 cup');
 }
+
 //call makeAddOns function
 makeAddOns();
+//define the counter for the for loop when we append the add ons
+var extraCounter = addOn.length;
+
+
 //renders the add ons to the page. this function will be called twice, rendering addOn set One and addOn set Two
-function renderAddOns() {
+function renderAddOns(addOnNumber) {
     //grabs the div element from the html
     var divEl = document.getElementById('add-ons');
     //create an unordered list that will hold all of the add on
-    var ulEl = document.createElement('ul');
-    ulEl.setAttribute('id', 'toppings-container');
+    var listEl = document.createElement('ul');
     //anchor the list to the div
-    divEl.appendChild(ulEl);
+    divEl.appendChild(listEl);
     //create a title for each of the two lists based on addOnNumber
-    // listEl.textContent = 'Add On '+ addOnNumber;
+    listEl.textContent = 'Add On ' + addOnNumber;
     //create a for loop to populate list. we also create the type, value, id and name for each input element
-    for (var i = 0; i < addOn.length; i++) {
+    for (var i = 0; i < extraCounter; i++) {
         var liEl = document.createElement('li');
         var inputEl = document.createElement('input');
         //the list items hold the input, which is where we assign all our data for later
-        inputEl.type = 'checkbox';
-        inputEl.setAttribute('class', 'toppings-checked')
-        inputEl.value = addOn[i].name;
+        inputEl.type = 'radio';
+        inputEl.value = addOn[i].name + addOnNumber;
         inputEl.id = [i] + 'id';
-        inputEl.name = 'add_ons' + i;
+        inputEl.name = 'add_ons' + addOnNumber;
         //the lable is linked to the input by the id, the lable holds the actual text content
         var labelEl = document.createElement('label');
         labelEl.htmlFor = [i] + 'id';
         labelEl.textContent = addOn[i].name;
+        listEl.appendChild(liEl);
         liEl.appendChild(inputEl);
         liEl.appendChild(labelEl);
-        ulEl.appendChild(liEl);
+        //inputEl.onclick = clickIngredient;
     }
 }
-renderAddOns();
+
+
+renderAddOns('one');
+renderAddOns('two');
+
 //BUTTONS
 //Set global variables:
 var reset = document.getElementById('reset');
@@ -133,6 +142,8 @@ reset.addEventListener('click', resetSurvey);
 function resetSurvey() {
     location.reload();
 }
+
+
 //define my variables for the event listener
 var cookieTypeSelected;
 var firstAddOn;
@@ -162,22 +173,44 @@ formEl.addEventListener('submit', function (event) {
             console.log(cookieTypeSelected);
         }
     }
+    //function that grabs the add ons.
+    //this is a hot mess, but it works
+    function addOnCookieChoice() {
+        console.log(event.target.add_onsone.value)
+        console.log(event.target.add_onstwo.value)
+        //we specified extraCounter a long time ago when we rendered our add on lists
+        for (var i = 0; i < extraCounter; i++) {
+            var itemNameOne = addOn[i].name + 'one' //sets up itemNameOne so it will match with the selected item
+            console.log('itemNameOne', itemNameOne);
+            if (event.target.add_onsone.value === itemNameOne) {
+                console.log('yay');
+                firstAddOn = addOn[i];
+                console.log(firstAddOn)
+            }
+        }
+        //same thing but for the second add on list
+        for (var j = 0; j < extraCounter; j++) {
+            var itemNameTwo = addOn[j].name + 'two'
+            console.log('itemNameTwo', itemNameTwo);
+            if (event.target.add_onstwo.value === itemNameTwo) {
+                console.log('yay');
+                secondAddOn = addOn[j];
+                console.log(secondAddOn)
+            }
+        }
+
+    }
     //calls the awesome functions we just made
     baseCookieChoice();
     addOnCookieChoice();
     renderRecipe();
 })
+
+
+
 //RENDER
 //YAY FUN STUFF
 function renderRecipe() {
-    if (localStorage.getItem('recipes')) {
-        var recipeJSON = localStorage.getItem('recipes', recipeJSON);
-        var recipeHistory = JSON.parse(recipeJSON);
-        console.log(recipeHistory);
-    }
-    else {
-        recipeHistory = [];
-    }
     //this section makes sure that the recipe re-renders on each submit
     recipeElCheck = document.getElementById('recipe');
     if (recipeElCheck) {
@@ -203,8 +236,6 @@ function renderRecipe() {
         recipe.ingredients.push(secondAddOn.amount + ' ' + secondAddOn.name);
         //this splices the addDirections property from the add ons into the instructions array
         //this one is for chocolate chip cookies
-        //TODO: add logic for new add-ins
-        //TODO: improve logic for amounts based on cookie selections
         if (recipe === chocolateChipRecipe) {
             //chocolate chips
             if (firstAddOn === addOn[0] || secondAddOn === addOn[0]) {
@@ -307,41 +338,22 @@ function renderRecipe() {
             olEl.appendChild(liEl);
             liEl.textContent = cookieTypeSelected.instructions[i];
         }
+
     }
 
     renderIngredientsList();
     renderInstructions();
     console.log('This code is running')
-    recipeHistory.push(recipe);
-    var recipeJSON = JSON.stringify(recipeHistory);
-    localStorage.setItem('recipes', recipeJSON);
 }
+
+
+//this is a printing button I broke
 //This is for specifically printing the instructions from the recipe
 function printDiv(printDiv) {
+
     var printContents = document.getElementById('recipe-box').innerHTML;
-    var w = window.open();
+    w = window.open();
     w.document.write(printContents);
     w.print();
     w.close();
-}
-var chocolateTopping = document.getElementById('toppings-container').addEventListener('change', checkToppings);
-var toppingsCounter = [];
-function checkToppings(event) {
-    for (var i = 0; i < addOn.length; i++) {
-        if (event.target.value === addOn[i].name) {
-            toppingsCounter.push(addOn[i]);
-        }
-    }
-
-    if (toppingsCounter.length > 2) {
-        alert('Please only check two toppings');
-        location.reload();
-    }
-}
-//function that grabs the add ons.
-//this is a hot mess, but it works
-function addOnCookieChoice() {
-
-    firstAddOn = toppingsCounter[0];
-    secondAddOn = toppingsCounter[1];
 }
